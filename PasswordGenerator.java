e73eb7d47dd328d9f6d81de250cc8fd6441654ee
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-08 21:02:08 +0530

    Create PasswordGenerator.java

---
+++ PasswordGenerator.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class PasswordGenerator {
+
+    public static String generatePassword(int length) {
+        return RandomStringUtils.random(length, true, true, true, true);
+    }
+
+    public static void main(String[] args) {
+        String password = generatePassword(12);
+        System.out.println(password);
+    }
+}
