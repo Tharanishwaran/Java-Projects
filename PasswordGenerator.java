aa739276b20ead29bbc09fbb97ef08d8ccd68bf3
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-08 21:06:01 +0530

    Update PasswordGenerator.java

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,18 +1,40 @@
-import org.apache.commons.lang3.RandomStringUtils; //apache commons libray file imported
-
 public class PasswordGenerator {
 
     public static String generatePassword(int length) {
-       
-        return RandomStringUtils.random(length, true, true, true, true); 
+        StringBuilder builder = new StringBuilder();
+      
+        Random random = new SecureRandom();
+        
+        for (int i = 0; i < length; i++) {
+        
+            int type = random.nextInt(4);
+            
+            switch (type) {
+                case 0:
+                    builder.append((char) (random.nextInt(26) + 'a'));
+                    break;
+            
+                case 1:
+                    builder.append((char) (random.nextInt(26) + 'A'));
+                    break;
+                
+                case 2:
+                    builder.append((char) (random.nextInt(10) + '0'));
+                    break;
+                
+                case 3:
+                    builder.append((char) (random.nextInt(33) + ' '));
+                    break;
+            }
+        }
+        
+        return builder.toString();
     }
 
     public static void main(String[] args) {
         
-        String password = generatePassword(12); 
+        String password = generatePassword(10);
         
-        System.out.println(password); 
-    
+        System.out.println(password);
     }
-
 }
